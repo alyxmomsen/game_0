5976b11f8fc6309a0a3c679f84cdb8c30bf4b4ac
alex <shizofranky@gmail.com>
2023-07-03 19:45:17 +0300

    explode file the gmaeobject.ts to two parts that is gameobject-properties.ts and gameobject-setters-getters.ts ; refact all via plugin  the 'prettier'

--- src/gameobjects/gameobject.ts
+++ src/gameobjects/gameobject.ts
@@ -16,150 +16,9 @@ import {
   GameObjectKinds,
   Position,
 } from "../library/types";
+import { GameObjectSttersGetters } from "./gameobject-part-2";
 
-export default abstract class GameObject {
-  private id: number;
-  private dateOfCreated: number;
-  private color: string;
-  private kind: GameObjectKinds;
-  private dimentions: Dimentions;
-  position: { x: number; y: number } | null = null;
-  private damaged: Damage[]; // в данный момент получаемыe уроны
-  private health: number;
-  private armor: Armor;
-  isDied: boolean;
-  protected attack: Attack;
-  movement: Movement;
-
-  /* ================================ */
-
-  /* -------- html --------- */
-
-  UI: GameObjectHTMLs;
-
-  HTLM_untit: HTML_unit;
-
-  /* ----------------------- */
-
-  setAttackDirection(keys: string[]) {
-    const up = keys.includes("ArrowUp");
-    const down = keys.includes("ArrowDown");
-    const left = keys.includes("ArrowLeft");
-    const right = keys.includes("ArrowRight");
-
-    // здесь есть баг, - если одновременно зажать две клавиши , то объект атакует сам себя
-
-    if (up || down || left || right) {
-      if (up && !down) {
-        this.attack.direction.y = -1;
-      } else if (!up && down) {
-        this.attack.direction.y = 1;
-      } else {
-        this.attack.direction.y = 0;
-      }
-
-      if (left && !right) {
-        this.attack.direction.x = -1;
-      } else if (!left && right) {
-        this.attack.direction.x = 1;
-      } else {
-        this.attack.direction.x = 0;
-      }
-
-      //временный дебаг, который иногда не срабатывает
-      if (this.attack.direction.x === 0 && this.attack.direction.y === 0) {
-        this.attack.direction.x = 1;
-      }
-    }
-  }
-
-  setWalkStepRate(value: number) {
-    this.movement.setTickInterval(value < 1 ? 1 : value);
-  }
-
-  // 'атака' на указаный объект
-  attackTo(
-    object: Bullet | GameObject | Enemy | Player | SupplyBox,
-    damage: Damage
-  ) {
-    object.damaged.push(new Damage(damage)); // добавляем объекту атаку в его очередь урона
-  }
-
-  getDamage(damage: number) {
-    if (this.armor.health > 0) {
-      this.armor.health -= Math.floor((damage / 100) * this.armor.dempher);
-      this.health -= Math.floor((damage / 100) * (100 - this.armor.dempher));
-    } else {
-      this.health -= damage;
-    }
-  }
-
-  // проверка на коллизию nextposition с переданными координатами
-  checkNextPositionColissionWith(subjectPostion: Position) {
-    if (
-      this.movement.nextPosition.x === subjectPostion.x &&
-      this.movement.nextPosition.y === subjectPostion.y
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // проверка на коллизию со стенами
-  checkCollissionWithFieldLimits({
-    width,
-    height,
-  }: {
-    width: number;
-    height: number;
-  }): boolean {
-    if (
-      this.movement.nextPosition.x >= width ||
-      this.movement.nextPosition.x < 0 ||
-      this.movement.nextPosition.y >= height ||
-      this.movement.nextPosition.y < 0
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  calculateNextPosition({ x, y }: { x: 1 | -1 | 0; y: 1 | -1 | 0 }) {
-    if (x !== 0 || y !== 0) {
-      this.movement.nextPosition.y = this.position.y + y;
-      this.movement.nextPosition.x = this.position.x + x;
-    }
-
-    if (x !== 0) {
-      this.movement.direction = { x, y };
-    } else if (y !== 0) {
-      this.movement.direction = { x, y };
-    }
-
-    this.movement.counterOfSteps += 1; // счетчик сделаных шагов. связан. используется
-  }
-
-  updatePosition() {
-    this.position.x = this.movement.nextPosition.x;
-    this.position.y = this.movement.nextPosition.y;
-  }
-
-  /// beta beta beta
-  calculateOwnDamageBySpeed() {
-    const damage = this.attack.ownDamage.value;
-    const stepRate = this.movement.getTickInterval();
-    const calculatedDamageValue = Math.floor(
-      damage / ((stepRate + 1000) / (1000 - stepRate))
-    );
-
-    return calculatedDamageValue;
-  }
-
-  setOwnDamageValue(value: number) {}
-
-  getOwnDamageValue() {}
+export default abstract class GameObject extends GameObjectSttersGetters {
 
   /* ====================== options ====================== */
 
@@ -302,6 +161,7 @@ export default abstract class GameObject {
     color,
     armor,
   }: GameObjectConstructor) {
+    super();
     this.movement = new Movement({
       stepRate: walkStepRate,
       direction,
