1946a11932078d9e2d59778580b87f956c8ac14c
alex <shizofranky@gmail.com>
2023-07-04 17:22:07 +0300

    refact

--- src/index.ts
+++ src/index.ts
@@ -7,24 +7,6 @@ import "./styles/main.css";
 
 (function () {
 
-  const linkedList = new LinkedList<Player> (new Player({id:0 , position:{x:0 , y:0} , weapons:[]})) ;
-
-  linkedList.append(new Player({id:1 , position:{x:0 , y:0} , weapons:[]})) ;
-  linkedList.append(new Player({id:2 , position:{x:0 , y:0} , weapons:[]})) ;
-  linkedList.append(new Player({id:3 , position:{x:0 , y:0} , weapons:[]})) ;
-  linkedList.append(new Player({id:4 , position:{x:0 , y:0} , weapons:[]})) ;
-
-
-  console.log(linkedList.currentIteration);
-  linkedList.next();
-  linkedList.next();
-  linkedList.next();
-  linkedList.next();
-  linkedList.next();
-
-  console.log(linkedList.currentIteration);
-
-
 
   const root = document.querySelector<HTMLElement>(".root");
   const gameFieldHTMLContainer =
